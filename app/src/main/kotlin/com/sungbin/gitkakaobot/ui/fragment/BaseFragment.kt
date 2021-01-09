e190ac355a74a91c724d1533ed1e6e7e3085b5c4
sungbin5304 <sungbin.dev@gmail.com>
2021-01-09 23:24:42 +0900

    이걸 어떻게 해야 할 까?

--- app/src/main/kotlin/com/sungbin/gitkakaobot/ui/fragment/BaseFragment.kt
+++ app/src/main/kotlin/com/sungbin/gitkakaobot/ui/fragment/BaseFragment.kt
@@ -1,7 +1,6 @@
 package com.sungbin.gitkakaobot.ui.fragment
 
 import androidx.fragment.app.Fragment
-import com.sungbin.gitkakaobot.ui.activity.DashboardActivity
 
 
 /**
@@ -12,6 +11,6 @@ import com.sungbin.gitkakaobot.ui.activity.DashboardActivity
 // todo: 탭 전환 애니메이션 살리기 -> 생명주기 써서 하면 될 거 같은데...
 abstract class BaseFragment : Fragment() {
 
-    val vm = (requireContext() as DashboardActivity).vm
+    // val vm = (requireContext() as DashboardActivity).vm
 
 }
