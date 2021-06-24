c38726db312ff35ba021b042e3fd472bf624a929
sungbin5304 <sungbin.dev@gmail.com>
2021-06-25 01:27:49 +0900

    Typography cleanup

--- app/src/main/kotlin/me/sungbin/gitmessengerbot/theme/Theme.kt
+++ app/src/main/kotlin/me/sungbin/gitmessengerbot/theme/Theme.kt
@@ -14,97 +14,14 @@ import androidx.compose.material.Typography
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import me.sungbin.gitmessengerbot.R
 
 val twiceLightGray = Color(236, 236, 236)
 val defaultFontFamily = FontFamily(Font(R.font.nanumbarungothic))
 
-val typography = Typography(
-    defaultFontFamily = defaultFontFamily,
-    h1 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Light,
-        fontSize = 96.sp,
-        letterSpacing = (-1.5).sp
-    ),
-    h2 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Light,
-        fontSize = 60.sp,
-        letterSpacing = (-0.5).sp
-    ),
-    h3 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 48.sp,
-        letterSpacing = 0.sp
-    ),
-    h4 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 34.sp,
-        letterSpacing = 0.25.sp
-    ),
-    h5 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 24.sp,
-        letterSpacing = 0.sp
-    ),
-    h6 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Medium,
-        fontSize = 20.sp,
-        letterSpacing = 0.15.sp
-    ),
-    subtitle1 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        letterSpacing = 0.15.sp
-    ),
-    subtitle2 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp,
-        letterSpacing = 0.1.sp
-    ),
-    body1 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        letterSpacing = 0.5.sp
-    ),
-    body2 = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
-        letterSpacing = 0.25.sp
-    ),
-    button = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp,
-        letterSpacing = 1.25.sp
-    ),
-    caption = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
-        letterSpacing = 0.4.sp
-    ),
-    overline = TextStyle(
-        fontFamily = defaultFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 10.sp,
-        letterSpacing = 1.5.sp
-    )
-)
+val typography = Typography(defaultFontFamily = defaultFontFamily)
 
 val colors = lightColors().copy(
     primary = Color(0xFF942ccc),
