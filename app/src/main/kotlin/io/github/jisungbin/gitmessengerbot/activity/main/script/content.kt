059019ff79ff00428446b941a75f540e3f0434c4
jisungbin <ji@sungb.in>
2021-07-24 01:32:52 +0900

    Code cleanup

--- app/src/main/kotlin/io/github/jisungbin/gitmessengerbot/activity/main/script/content.kt
+++ app/src/main/kotlin/io/github/jisungbin/gitmessengerbot/activity/main/script/content.kt
@@ -2,7 +2,7 @@
  * GitMessengerBot © 2021 지성빈 & 구환. all rights reserved.
  * GitMessengerBot license is under the GPL-3.0.
  *
- * [script.kt] created by Ji Sungbin on 21. 6. 19. 오후 10:56.
+ * [content.kt] created by Ji Sungbin on 21. 6. 19. 오후 10:56.
  *
  * Please see: https://github.com/GitMessengerBot/GitMessengerBot-Android/blob/master/LICENSE.
  */
