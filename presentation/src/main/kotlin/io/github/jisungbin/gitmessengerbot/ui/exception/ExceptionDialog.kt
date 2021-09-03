19d3b0852d8f6c3b2c7386cbe77fc79876291c27
jisungbin <ji@sungb.in>
2021-09-04 00:56:13 +0900

    Feat: 에러 다이얼로그 추가

--- presentation/src/main/kotlin/io/github/jisungbin/gitmessengerbot/ui/exception/ExceptionDialog.kt
+++ presentation/src/main/kotlin/io/github/jisungbin/gitmessengerbot/ui/exception/ExceptionDialog.kt
@@ -9,9 +9,10 @@
 
 package io.github.jisungbin.gitmessengerbot.ui.exception
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.OutlinedButton
@@ -19,15 +20,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.airbnb.lottie.compose.LottieAnimation
@@ -36,78 +34,46 @@ import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import io.github.jisungbin.gitmessengerbot.R
 import io.github.jisungbin.gitmessengerbot.common.core.Util
-import io.github.jisungbin.gitmessengerbot.common.exception.CoreException
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ExceptionDialog(visible: MutableState<Boolean>, exception: Exception) {
     if (visible.value) {
         val context = LocalContext.current
-        val contents = listOf("눈덩이", "돌덩이", "나뭇가지", "새똥", "나뭇잎", "흙더미")
-        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.something_error))
+        val contents = listOf("눈덩이", "돌덩이", "나뭇가지", "새똥", "나뭇잎", "흙더미")
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.something_error_2))
 
         AlertDialog(
-            modifier = Modifier.size(350.dp),
             onDismissRequest = { visible.value = false },
             properties = DialogProperties(usePlatformDefaultWidth = false),
             confirmButton = {
                 exception.message?.let { exceptionMessage ->
                     OutlinedButton(onClick = { Util.copy(context, exceptionMessage) }) {
-                        Text(text = "에러 복사")
+                        Text(text = "에러 복사")
                     }
                 }
             },
-            text = {
-                Column(
-                    modifier = Modifier.size(300.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+            shape = RoundedCornerShape(30.dp),
+            title = {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.wrapContentSize()
                 ) {
                     LottieAnimation(
-                        modifier = Modifier.size(200.dp),
                         iterations = LottieConstants.IterateForever,
                         composition = composition,
-                    )
-                    Text(
-                        text = "깃메봇이가 예상치 못한 ${contents.random()}에 맞았어요 \uD83E\uDD72\n${exception.message}",
-                        textAlign = TextAlign.Center,
-                        color = Color.Black
+                        modifier = Modifier.size(200.dp)
                     )
                 }
             },
-            shape = RoundedCornerShape(30.dp)
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun PreviewExceptionDialog() {
-    val `true` = remember { mutableStateOf(true) }
-    ExceptionDialog(visible = `true`, exception = CoreException("TODO"))
-}
-
-@Preview
-@Composable
-private fun PreviewExceptionColumn() {
-    val exception = CoreException("TODO")
-    val contents = listOf("눈덩이", "돌덩이", "나뭇가지", "새똥", "나뭇잎", "흙더미")
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.something_error))
-
-    Column(
-        modifier = Modifier.size(250.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        LottieAnimation(
-            modifier = Modifier.size(200.dp),
-            iterations = LottieConstants.IterateForever,
-            composition = composition,
-        )
-        Text(
-            text = "깃메봇이가 예상치 못한 ${contents.random()}에 맞았어요 \uD83E\uDD72\n$exception",
-            textAlign = TextAlign.Center,
-            color = Color.Black
+            text = {
+                Text(
+                    text = "깃메봇이가 예상치 못한 ${contents.random()}에 맞았어요 \uD83E\uDD72\n${exception.message}",
+                    textAlign = TextAlign.Center,
+                    color = Color.Black,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         )
     }
 }
