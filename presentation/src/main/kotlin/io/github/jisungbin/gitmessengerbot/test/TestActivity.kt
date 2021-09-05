f92d87780d800aecd934df42c811e615a7458078
jisungbin <ji@sungb.in>
2021-09-05 22:04:32 +0900

    Update: 테스트 클래스

--- presentation/src/main/kotlin/io/github/jisungbin/gitmessengerbot/test/TestActivity.kt
+++ presentation/src/main/kotlin/io/github/jisungbin/gitmessengerbot/test/TestActivity.kt
@@ -13,9 +13,22 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.jisungbin.gitmessengerbot.activity.editor.js.CommitHistoryItem
@@ -35,21 +48,42 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class TestActivity : ComponentActivity() {
 
-    val repoName = "test"
-    val gitUser: GithubData = Storage.read(GithubConfig.DataPath, null)?.toModel()
+    private val repoName = "test"
+    private val gitUser: GithubData = Storage.read(GithubConfig.DataPath, null)?.toModel()
         ?: throw PresentationException("GithubConfig.DataPath data value is null.")
     val repoPath = "script.ts"
-    val repoDescription = GithubConfig.DefaultRepoDescription // TODO
+    private val repoDescription = GithubConfig.DefaultRepoDescription // TODO
     val repoBranch = AppConfig.appValue.gitDefaultBranch // TODO
     val repo = GithubRepo(name = repoName, description = repoDescription)
+
+    private val logs = mutableStateListOf("TestActivity")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text("Test Activity")
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(30.dp)
+            ) {
+                items(logs) { log ->
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Divider(modifier = Modifier.fillMaxWidth())
+                        Text(text = log, color = Color.Black, fontSize = 15.sp)
+                        Divider(modifier = Modifier.fillMaxWidth())
+                    }
+                }
+            }
             Test()
         }
+    }
+
+    private fun log(message: Any) {
+        logs.add(message.toString())
     }
 
     @SuppressLint("CoroutineCreationDuringComposition")
@@ -64,11 +98,10 @@ class TestActivity : ComponentActivity() {
                 .collect { commitListResult ->
                     commitListResult.doWhen(
                         onSuccess = { commitLists ->
-                            println("commitListResult success: ")
-                            println(commitLists.commitList)
+                            log("commitListResult success: ${commitLists.commitList}")
                             val commitHistory = mutableListOf<CommitHistoryItem>()
                             commitLists.commitList.forEach { commitList ->
-                                println("forEach: $commitList")
+                                log("forEach: $commitList")
                                 vm.getCommitContent(
                                     ownerName = gitUser.userName,
                                     repoName = repoName,
@@ -76,9 +109,9 @@ class TestActivity : ComponentActivity() {
                                 ).collect { commitContentResult ->
                                     commitContentResult.doWhen(
                                         onSuccess = { commitContents ->
-                                            println("commitContents result: $commitContents")
+                                            log("commitContents result: $commitContents")
                                             commitContents.files.forEach { commitContentItem ->
-                                                println("innerForEach: $commitContentItem")
+                                                log("innerForEach: $commitContentItem")
                                                 commitHistory.add(
                                                     CommitHistoryItem(
                                                         key = commitList,
@@ -86,19 +119,19 @@ class TestActivity : ComponentActivity() {
                                                     )
                                                 )
                                             }
-                                            println("TASK ENDED")
+                                            log("TASK ENDED")
                                         },
                                         onFail = { exception ->
-                                            println("ERROR")
-                                            println(exception)
+                                            log("ERROR")
+                                            log(exception)
                                         }
                                     )
                                 }
                             }
                         },
                         onFail = { exception ->
-                            println(exception)
-                            println("ERROR")
+                            log(exception)
+                            log("ERROR")
                         }
                     )
                 }
