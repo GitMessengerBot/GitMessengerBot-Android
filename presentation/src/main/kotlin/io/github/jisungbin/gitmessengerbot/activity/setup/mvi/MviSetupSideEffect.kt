4e3343c9106b230891a51fe0bc2c11d6dfda807c
jisungbin <ji@sungb.in>
2021-09-19 17:02:39 +0900

    Refactor: Apply MVI (2)

--- presentation/src/main/kotlin/io/github/jisungbin/gitmessengerbot/activity/setup/mvi/MviSetupSideEffect.kt
+++ presentation/src/main/kotlin/io/github/jisungbin/gitmessengerbot/activity/setup/mvi/MviSetupSideEffect.kt
@@ -2,16 +2,15 @@
  * GitMessengerBot © 2021 지성빈 & 구환. all rights reserved.
  * GitMessengerBot license is under the GPL-3.0.
  *
- * [SetupSideEffect.kt] created by Ji Sungbin on 21. 9. 18. 오후 9:14
+ * [MviSetupSideEffect.kt] created by Ji Sungbin on 21. 9. 19. 오후 4:57
  *
  * Please see: https://github.com/GitMessengerBot/GitMessengerBot-Android/blob/master/LICENSE.
  */
 
-package io.github.jisungbin.gitmessengerbot.activity.setup
+package io.github.jisungbin.gitmessengerbot.activity.setup.mvi
 
 import io.github.jisungbin.gitmessengerbot.domain.github.model.user.GithubData
 
 sealed class MviSetupSideEffect {
-    data class Error(val exception: Exception) : MviSetupSideEffect()
     data class SaveData(val data: GithubData) : MviSetupSideEffect()
 }
