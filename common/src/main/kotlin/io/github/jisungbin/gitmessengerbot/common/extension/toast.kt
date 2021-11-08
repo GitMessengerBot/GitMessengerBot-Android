259f5a38c2a8f5be8d97644a0ae3c0022ab413a9
jisungbin <ji@sungb.in>
2021-11-08 20:21:59 +0900

    Refactoring - 10

--- common/src/main/kotlin/io/github/jisungbin/gitmessengerbot/common/extension/toast.kt
+++ common/src/main/kotlin/io/github/jisungbin/gitmessengerbot/common/extension/toast.kt
@@ -2,7 +2,7 @@
  * GitMessengerBot © 2021 지성빈 & 구환. all rights reserved.
  * GitMessengerBot license is under the GPL-3.0.
  *
- * [Activity.kt] created by Ji Sungbin on 21. 8. 30. 오후 5:05
+ * [toast.kt] created by Ji Sungbin on 21. 8. 30. 오후 5:06
  *
  * Please see: https://github.com/GitMessengerBot/GitMessengerBot-Android/blob/master/LICENSE.
  */
@@ -10,8 +10,15 @@
 package io.github.jisungbin.gitmessengerbot.common.extension
 
 import android.app.Activity
+import android.content.Context
 import android.widget.Toast
 
 fun Activity.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
-    toast(this, message, length)
+    runOnUiThread {
+        toast(this, message, length)
+    }
+}
+
+fun toast(context: Context, message: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(context, message, length).show()
 }
