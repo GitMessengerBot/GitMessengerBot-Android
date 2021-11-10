2ebef6e6590c1fc60ae04f9efa85aab60fb66cb9
jisungbin <ji@sungb.in>
2021-11-10 20:42:23 +0900

    Gradle version up

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -28,7 +28,7 @@ object Versions {
         const val Kotlin = "1.5.31"
         const val CoreKtx = "1.6.0"
         const val Coroutines = "1.5.1"
-        const val Gradle = "7.1.0-beta02"
+        const val Gradle = "7.1.0-beta03"
     }
 
     object Bot {
