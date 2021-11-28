fd62ae39da62e9dbccf19ead35a224cb6156f26a
jisungbin <ji@sungb.in>
2021-11-28 15:11:14 +0900

    Chore: update gradle version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -29,7 +29,7 @@ object Versions {
         const val Kotlin = "1.5.31"
         const val CoreKtx = "1.7.0"
         const val Coroutines = "1.5.1"
-        const val Gradle = "7.1.0-beta03"
+        const val Gradle = "7.1.0-beta04"
         const val GoogleService = "4.3.10"
     }
 
